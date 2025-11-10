b78e650c66f7b5ce0d0c544497c9ae0b41d062f0
문경림 <naru@takeone.co.kr>
2025-11-10 19:17:59 +0900

    기본 스크린 설정 추가 , 이미지 추가

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import SignInScreen from './screens/SignInScreen';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+const App = () => {
+    {/* 💡 SafeAreaView에 flex: 1과 backgroundColor를 가진 styles.container 적용 
+        SafeAreaProvider가 안전 영역 정보를 설정합니다.
+
+        SafeAreaView가 그 정보를 받아 상태 표시줄과 홈 인디케이터 영역을 제외한 곳에 콘텐츠를 배치하며, 
+        여기에 styles.container에 정의된 흰색 배경이 화면 전체에 걸쳐 적용될 것입니다.*/}
+        
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView style={styles.container}> 
+                <StatusBar style="dark" />
+                <Text style={{
+                    fontSize: 60,
+                    color: "green",
+                }}>TODO App</Text>
+                <SignInScreen />
+            </SafeAreaView>
+        </SafeAreaProvider>
+    );
+};
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
+
+export default App;
---
+++ src/screens/SignInScreen.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+const SignInScreen = () => {
+    return (
+        <View style ={styles.container}>
+            <Text style ={{color: "red"}}>SignInScreen</Text>
+        </View>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
+
+export default SignInScreen;
